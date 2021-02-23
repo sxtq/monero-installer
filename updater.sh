1e275dfa4a33f51f34c7db1c9bf333c90c726461
882wZS6Ps7 <50447463+882wZS6Ps7@users.noreply.github.com>
2021-02-23 22:21:02 +0000

    Update updater.sh
    adds different colors to help tell whats good or bad

--- updater.sh
+++ updater.sh
@@ -26,17 +26,27 @@ url2=https://downloads.getmonero.org/cli/linuxarm8
 
 #Used for printing text on the screen
 print () {
-  echo -e "\033[1;33m$1\033[0m"
+  nc='\033[0m'
+  if [ "$2" = "green" ]; then
+    color='\033[1;32m'
+  fi
+  if [ "$2" = "yellow" ]; then
+    color='\033[1;33m'
+  fi
+  if [ "$2" = "red" ]; then
+    color='\033[1;31m'
+  fi
+  echo -e "${color}$1${nc}"
 }
 
 #This makes the backup and removes old files then extracts the verifed binary to the xmr directory
 updater () {
   if [ "$backup" = "1" ]; then
-    print "Moving current version to backup file"
+    print "Moving current version to backup file" yellow
     rm -dr "$wd.bk"
     cp -r "$wd" "$wd.bk"
   fi
-  print "Extracting binary to $wd"
+  print "Extracting binary to $wd" yellow
   mkdir "$wd"
   tar -xjvf "$tmpdir/$a1" -C "$wd" --strip-components=1
   rm "$tmpdir/$keyname" "$tmpdir/$hashfile" "$tmpdir/$a1"
@@ -46,32 +56,32 @@ updater () {
 verifier () {
   mkdir "$tmpdir"
   rm "$tmpdir/$keyname" "$tmpdir/$hashfile"
-  print "Downloading signing key and verifying signing key"
+  print "Downloading signing key and verifying signing key" yellow
   wget -O "$tmpdir/$keyname" "$keyurl"
   if gpg --keyid-format long --with-fingerprint "$tmpdir/$keyname" | grep -q "$fingerprint"; then
-    print "Good signing key importing signing key"
+    print "Good signing key importing signing key" green
     gpg --import "$tmpdir/$keyname"
-    print "Downloading then checking the hash file"
+    print "Downloading then checking the hash file" yellow
     wget -O "$tmpdir/$hashfile" "$hashurl"
     if gpg --verify "$tmpdir/$hashfile"; then
       checkversion
       hash0=$(sed -n "$line"p "$tmpdir/$hashfile" | cut -f 1 -d ' ')
-      print "The text file hash for $a1 is $hash0 downloading binary"
+      print "The text file hash for $a1 is $hash0 downloading binary" yellow
       rm "$tmpdir/$a1"
       wget -P "$tmpdir" "$url"
       hash1=$(shasum -a 256 "$tmpdir/$a1" | cut -f 1 -d ' ')
-      print "The binary hash for $a1 is $hash1 checking match"
+      print "The binary hash for $a1 is $hash1 checking match" yellow
       if [ "$hash1" = "$hash0" ]; then
-        print "Good match starting update"
+        print "Good match starting update" green
         updater
       else
-        print "Failed match stopping updater"
+        print "Failed match stopping updater" red
       fi
     else
-      print "Failed to verify hashes stopping updater"
+      print "Failed to verify hashes stopping updater" red
     fi
   else
-    print "Failed to verify signing key stopping updater"
+    print "Failed to verify signing key stopping updater" red
   fi
 }
 
@@ -82,26 +92,26 @@ checkversion () {
     a1=linux64
     url="$url0"
     line=$(grep -n monero-linux-x64 "$tmpdir/$hashfile" | cut -d : -f 1)
-    print "Monerod version set to $a1"
+    print "Monerod version set to $a1" green
   fi
   if [ "$version" = 'armv7l' ] || [ "$version" = '2' ]; then
     a1=linuxarm7
     url="$url1"
     line=$(grep -n monero-linux-armv7 "$tmpdir/$hashfile" | cut -d : -f 1)
-    print "Monerod version set to $a1"
+    print "Monerod version set to $a1" green
   fi
   if [ "$version" = 'armv8l' ] || [ "$version" = '3' ]; then
     a1=linuxarm8
     url="$url2"
     line=$(grep -n monero-linux-armv8 "$tmpdir/$hashfile" | cut -d : -f 1)
-    print "Monerod version set to $a1"
+    print "Monerod version set to $a1" green
   fi
   if [ "$line" = '0' ]; then
-    print "Failed to detect version"
-    print "1 = x64, 2 = armv7, 3 = armv8, Enter nothing to exit"
+    print "Failed to detect version" red
+    print "1 = x64, 2 = armv7, 3 = armv8, Enter nothing to exit" yellow
     read -r -p "Select a version [1/2/3]: " version
     if [ "$version" = '' ]; then
-      print "No version selected exiting"
+      print "No version selected exiting" red
       rm "$tmpdir/$keyname" "$tmpdir/$hashfile"
       exit 1
     fi
@@ -116,9 +126,9 @@ checkupdate () {
     cvrs=1.3.3
     lvrs=$(curl -s https://github.com/882wZS6Ps7/Monero-CLI-bash-updater/releases/latest | sed 's/.*v\(.*\)">.*/\1/')
     if [ "$lvrs" = "$cvrs" ]; then
-      print "This script is up to date current version is: $cvrs"
+      print "This script is up to date current version is: $cvrs" green
     else
-      print "This script is outdated latest version: $lvrs Current version: $cvrs"
+      print "This script is outdated latest version: $lvrs Current version: $cvrs" red
     fi
   fi
   if [ "$checker1" = "0" ]; then
@@ -134,26 +144,26 @@ checkupdate () {
     w="install"
   fi
   if [ "$current" = "$latest" ]; then
-    print "No update avalible latest version: $latest Current version: $current"
+    print "No update avalible latest version: $latest Current version: $current" green
     read -r -p "Would you like to update anyways? [N/y]: " output
     if [ "$output" = 'y' ] || [ "$output" = 'Y' ]; then
-      print "Starting updater"
+      print "Starting updater" yellow
       verifier
     else
       return 0
     fi
   else
-    print "Update avalible latest version: $latest Current version: $current"
+    print "Update avalible latest version: $latest Current version: $current" red
     read -r -p "Would you like to $w? [Y/n]: " output
     if [ "$output" = 'n' ] || [ "$output" = 'N' ]; then
       return 0
     else
-      print "Starting updater"
+      print "Starting updater" yellow
       verifier
     fi
   fi
 }
 
-print "Current fingerprint: $fingerprint"
-print "Current Directory: $wd"
+print "Current fingerprint: $fingerprint" yellow
+print "Current Directory: $wd" yellow
 checkupdate
