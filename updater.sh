644aea8fa28338de8aaa48a381dc9448997ae080
882wZS6Ps7 <42ehr2kwo6w2o825c27u7o9wi@qubitmail.com>
2021-03-10 02:11:28 -0600

    Adds better var names

--- updater.sh
+++ updater.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Version 1.3.4
-dirname="xmr" #Name of directory that contains monero software files (make it whatever you want)
+directory_name="xmr" #Name of directory that contains monero software files (make it whatever you want)
 version=$(uname -m) #version=1 for 64-bit, 2 for arm7 and 3 for arm8 or version=$(uname -m) for auto detect
-directory=$(printf "%q\n" "$(pwd)" | sed 's/\/'$dirname'//g')
-wd="$directory/$dirname" #To set manually use this example wd=/home/myUser/xmr
-tmpdir="/tmp/xmr-75RvX3g3P" #This is where the hashes.txt, binary file and sigining key will be stored while the script is running.
+directory=$(printf "%q\n" "$(pwd)" | sed 's/\/'$directory_name'//g')
+working_directory="$directory/$directory_name" #To set manually use this example working_directory=/home/myUser/xmr
+temp_directory="/tmp/xmr-75RvX3g3P" #This is where the hashes.txt, binary file and sigining key will be stored while the script is running.
 
 checker0=1 #Change this number to 0 to avoid checking for a script update
 checker1=1 #Change this number to 0 to avoid checking for a monero update (Just download and install)
@@ -14,21 +14,21 @@ backup=1 #Change this to 0 to not backup any files (If 0 script wont touch walle
 #Match the fingerprint below with the one here
 #https://web.getmonero.org/resources/user-guides/verification-allos-advanced.html#22-verify-signing-key
 fingerprint="81AC 591F E9C4 B65C 5806  AFC3 F0AF 4D46 2A0B DF92"
-keyurl=https://raw.githubusercontent.com/monero-project/monero/master/utils/gpg_keys/binaryfate.asc #Keyfile download URL
-keyname=binaryfate.asc #Key file name (Used to help the script locate the file)
-hashurl=https://www.getmonero.org/downloads/hashes.txt #Hash file download URL
-hashfile=hashes.txt #Hash file name (Used to help the script locate the file)
+key_url=https://raw.githubusercontent.com/monero-project/monero/master/utils/gpg_keys/binaryfate.asc #Keyfile download URL
+key_name=binaryfate.asc #Key file name (Used to help the script locate the file)
+hash_url=https://www.getmonero.org/downloads/hashes.txt #Hash file download URL
+hash_file=hashes.txt #Hash file name (Used to help the script locate the file)
 
 #x86_64 CLI URL
-url0=https://downloads.getmonero.org/cli/linux64
+url_linux64=https://downloads.getmonero.org/cli/linux64
 #arm7 CLI URL
-url1=https://downloads.getmonero.org/cli/linuxarm7
+url_linuxarm7=https://downloads.getmonero.org/cli/linuxarm7
 #arm8 CLI URL
-url2=https://downloads.getmonero.org/cli/linuxarm8
+url_linuxarm8=https://downloads.getmonero.org/cli/linuxarm8
 
 #Used for printing text on the screen
 print () {
-  nc='\033[0m'
+  no_color='\033[0m'
   if [ "$2" = "green" ]; then
     color='\033[1;32m'
   elif [ "$2" = "yellow" ]; then
@@ -36,47 +36,47 @@ print () {
   elif [ "$2" = "red" ]; then
     color='\033[1;31m'
   fi
-  echo -e "${color}$1${nc}"
+  echo -e "${color}$1${no_color}"
 }
 
 #This makes the backup and removes old files then extracts the verifed binary to the xmr directory
 updater () {
   if pgrep monerod; then #Stops monerod to make sure it does not corrupt database when updating
     print "Stopping monerod to protect database during upgrade" yellow
-    "$wd"/monerod exit
+    "$working_directory"/monerod exit
     sleep 3
   fi
   if [ "$backup" = "1" ]; then
     print "Moving current version to backup file" yellow
-    rm -dr "$wd.bk"
-    cp -r "$wd" "$wd.bk"
+    rm -dr "$working_directory.bk"
+    cp -r "$working_directory" "$working_directory.bk"
   fi
-  print "Extracting binary to $wd" yellow
-  mkdir "$wd"
-  tar -xjvf "$tmpdir/$a1" -C "$wd" --strip-components=1
+  print "Extracting binary to $working_directory" yellow
+  mkdir "$working_directory"
+  tar -xjvf "$temp_directory/$binary_name" -C "$working_directory" --strip-components=1
   print "Removing temp files (Binary/Hash file/Signing key)"
-  rm "$tmpdir/$keyname" "$tmpdir/$hashfile" "$tmpdir/$a1"
+  rm "$temp_directory/$key_name" "$temp_directory/$hash_file" "$temp_directory/$binary_name"
 }
 
 #This verifies the binary, signing key and hash file
 verifier () {
-  mkdir "$tmpdir"
-  rm "$tmpdir/$keyname" "$tmpdir/$hashfile"
+  mkdir "$temp_directory"
+  rm "$temp_directory/$key_name" "$temp_directory/$hash_file"
   print "Downloading signing key and verifying signing key" yellow
-  wget -O "$tmpdir/$keyname" "$keyurl"
-  if gpg --keyid-format long --with-fingerprint "$tmpdir/$keyname" | grep -q "$fingerprint"; then
+  wget -O "$temp_directory/$key_name" "$key_url"
+  if gpg --keyid-format long --with-fingerprint "$temp_directory/$key_name" | grep -q "$fingerprint"; then
     print "Good signing key importing signing key" green
-    gpg --import "$tmpdir/$keyname"
+    gpg --import "$temp_directory/$key_name"
     print "Downloading then checking the hash file" yellow
-    wget -O "$tmpdir/$hashfile" "$hashurl"
-    if gpg --verify "$tmpdir/$hashfile"; then
-      line=$(grep -n "$locate" "$tmpdir/$hashfile" | cut -d : -f 1)
-      hash0=$(sed -n "$line"p "$tmpdir/$hashfile" | cut -f 1 -d ' ')
-      print "The text file hash for $a1 is $hash0 downloading binary" yellow
-      rm "$tmpdir/$a1"
-      wget -P "$tmpdir" "$url"
-      hash1=$(shasum -a 256 "$tmpdir/$a1" | cut -f 1 -d ' ')
-      print "The binary hash for $a1 is $hash1 checking match" yellow
+    wget -O "$temp_directory/$hash_file" "$hash_url"
+    if gpg --verify "$temp_directory/$hash_file"; then
+      line=$(grep -n "$locate" "$temp_directory/$hash_file" | cut -d : -f 1)
+      hash0=$(sed -n "$line"p "$temp_directory/$hash_file" | cut -f 1 -d ' ')
+      print "The text file hash for $binary_name is $hash0 downloading binary" yellow
+      rm "$temp_directory/$binary_name"
+      wget -P "$temp_directory" "$url"
+      hash1=$(shasum -a 256 "$temp_directory/$binary_name" | cut -f 1 -d ' ')
+      print "The binary hash for $binary_name is $hash1 checking match" yellow
       if [ "$hash1" = "$hash0" ]; then
         print "Good match starting update" green
         updater
@@ -94,27 +94,27 @@ verifier () {
 #This is checks what version the verifier needs to download and  what line is needed in the hash file
 checkversion () {
   if [ "$version" = 'x86_64' ] || [ "$version" = '1' ]; then
-    a1=linux64
-    url="$url0"
-    print "Monerod version set to $a1" green
+    binary_name=linux64
+    url="$url_linux64"
+    print "Monerod version set to $binary_name" green
     locate="monero-linux-x64"
   elif [ "$version" = 'armv7l' ] || [ "$version" = '2' ]; then
-    a1=linuxarm7
-    url="$url1"
-    print "Monerod version set to $a1" green
+    binary_name=linuxarm7
+    url="$url_linuxarm7"
+    print "Monerod version set to $binary_name" green
     locate="monero-linux-armv7"
   elif [ "$version" = 'armv8l' ] || [ "$version" = '3' ]; then
-    a1=linuxarm8
-    url="$url2"
-    print "Monerod version set to $a1" green
+    binary_name=linuxarm8
+    url="$url_linuxarm8"
+    print "Monerod version set to $binary_name" green
     locate="monero-linux-armv8"
-  elif [ -z "$a1" ]; then
+  elif [ -z "$binary_name" ]; then
     print "Failed to detect version" red
     print "1 = x64, 2 = armv7, 3 = armv8, Enter nothing to exit" yellow
     read -r -p "Select a version [1/2/3]: " version
     if [ -z "$version" ]; then
       print "No version selected exiting" red
-      rm "$tmpdir/$keyname" "$tmpdir/$hashfile"
+      rm "$temp_directory/$key_name" "$temp_directory/$hash_file"
       exit 1
     fi
     checkversion
@@ -124,18 +124,18 @@ checkversion () {
 #This will check for an update by looking at the github release page for the latest version
 checkupdate () {
   if [ "$checker0" = "1" ]; then #Checks for updates to this script, this can be turned off above.
-    cvrs=1.3.4
-    lvrs=$(curl -s https://github.com/882wZS6Ps7/Monero-CLI-bash-updater/releases/latest | sed 's/.*v\(.*\)">.*/\1/')
-    if [ "$lvrs" = "$cvrs" ]; then
-      print "[Script] No update avalible latest: $lvrs Current: $cvrs" green
+    current_version=1.3.4
+    latest_version=$(curl -s https://github.com/882wZS6Ps7/Monero-CLI-bash-updater/releases/latest | sed 's/.*v\(.*\)">.*/\1/')
+    if [ "$latest_version" = "$current_version" ]; then
+      print "[Script] No update avalible latest: $latest_version Current: $current_version" green
     else
-      print "[Script] Update avalible latest: $lvrs Current: $cvrs" red
+      print "[Script] Update avalible latest: $latest_version Current: $current_version" red
     fi
   fi
   if [ "$checker1" = "1" ]; then
     latest=$(curl -s https://github.com/monero-project/monero/releases/latest | sed 's/.*v\(.*\)">.*/\1/')
-    if [ -f "$wd/monerod" ]; then
-      current=$("$wd"/monerod --version | sed 's/.*v\(.*\)-.*/\1/')
+    if [ -f "$working_directory/monerod" ]; then
+      current=$("$working_directory"/monerod --version | sed 's/.*v\(.*\)-.*/\1/')
     else
       current="Not installed"
     fi
@@ -165,11 +165,11 @@ checkupdate () {
 
 checkversion
 print "Current fingerprint: $fingerprint" yellow
-print "Current install directory: $wd" yellow
-print "Current temp directory: $tmpdir" yellow
+print "Current install directory: $working_directory" yellow
+print "Current temp directory: $temp_directory" yellow
 if [ "$backup" = "1" ]; then
-  print "Backup ON script will copy /$dirname/ files to $wd.bk" yellow
+  print "Backup ON script will copy /$directory_name/ files to $working_directory.bk" yellow
 else
-  print "Backup OFF script will not backup /$dirname/ files" yellow
+  print "Backup OFF script will not backup /$directory_name/ files" yellow
 fi
 checkupdate
