2dd89ff0d1514510e396daefdbd656d26122e60e
sxtq <admin@qubitmail.com>
2021-05-09 18:08:14 +0000

    Update updater.sh
    Removes testing comment

--- updater.sh
+++ updater.sh
@@ -2,7 +2,7 @@
 
 #Version 1.3.5
 directory_name="xmr" #Name of directory that contains monero software files (make it whatever you want)
-version= #$(uname -m) #version=1 for 64-bit, 2 for arm7, 3 for arm8 and 4, for android arm8, 5 for linux32 or version=$(uname -m) for auto detect
+version=$(uname -m) #version=1 for 64-bit, 2 for arm7, 3 for arm8 and 4, for android arm8, 5 for linux32 or version=$(uname -m) for auto detect
 directory=$(printf "%q\n" "$(pwd)" | sed 's/\/'$directory_name'//g')
 working_directory="$directory/$directory_name" #To set manually use this example working_directory=/home/myUser/xmr
 temp_directory="$directory/tmp-xmr-483" #This is where the hashes.txt, binary file and sigining key will be stored while the script is running.
